3a9645cb3b041ee708f6521a056e049618f20e1a
Vitalii Kuzmenko <vitalii_kuzmenko1@epam.com>
2021-11-03 20:15:08 +0200

    minor change

--- email/src/main/java/com/vkuzmenko/pizza/email/Constants.java
+++ email/src/main/java/com/vkuzmenko/pizza/email/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String API_URL = "/api";
     public static final String API_URL_EMAIL = "email";
     public static final String API_URL_ORDER_EMAIL = "orderEmail";
-    public static final String ADMIN_EMAIL = "azeroth999@gmail.com";
+    public static final String ADMIN_EMAIL = "admin@email.com";
     public static final String EMAIL_CONTACT_SUBJECT = "Contact form";
 }
 
